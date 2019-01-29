ef125b45e4566733706eabc8f1a90c2e4fc85e8d
Shu Uesugi <shu@chibicode.com>
2019-01-29 07:54:09 -0800

    update og

--- src/components/head.js
+++ src/components/head.js
@@ -3,8 +3,7 @@ import NextHead from 'next/head'
 
 export const title = '『ファクトフルネス』風？日本の事実ニホンザルクイズ'
 export const description = '日本の事実にまつわる12の質問にチャレンジ'
-export const imageUrl =
-  'https://factquiz-jp.chibicode.com/static/images/og-jp.png'
+export const imageUrl = 'https://factquiz-jp.chibicode.com/static/images/og.png'
 
 const Head = () => (
   <NextHead>
