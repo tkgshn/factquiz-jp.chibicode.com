f7f917aecea36f053b5e4a72e2c918626e85dde1
Shu Uesugi <shu@chibicode.com>
2019-10-19 11:07:19 -0700

    .

--- src/components/amazon-link.js
+++ src/components/amazon-link.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ExternalLink from './external-link'
 
 const AmazonLink = props => (
-  <ExternalLink href="https://amzn.to/2FRQueY" {...props} />
+  <ExternalLink href="https://www.amazon.co.jp/dp/4822289605/ref=cm_sw_em_r_mt_dp_U_h20QDbG5PNQK4" {...props} />
 )
 
 export default AmazonLink
