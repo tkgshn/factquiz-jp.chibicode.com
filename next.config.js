1d8b20b541992c18c6fb26fb0f0f78d79773166d
Shunsuke Takagi (MacBook Pro) <takagishunsuke1129@gmail.com>
2022-02-15 09:40:30 +0900

    ルーティングを設定

--- next.config.js
+++ next.config.js
@@ -3,3 +3,13 @@ const withCSS = require('@zeit/next-css')
 const config = withCSS()
 
 module.exports = config
+
+const urlPrefix = process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : ''
+
+module.exports = {
+  // ...
+  assetPrefix: urlPrefix,
+  basePath: urlPrefix,
+  trailingSlash: true,
+  // ...
+};
